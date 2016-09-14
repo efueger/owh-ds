45437bfded64526f6f9e87f0481488924d63cefc
gjanakiram <janakiram.gollapudi@semanticbits.com>
2016-09-14 16:57:44 -0400

    added test cases to verify visualization legends and elements

--- software/owh/client/test/e2e-tests/search/mortality.spec.js
+++ software/owh/client/test/e2e-tests/search/mortality.spec.js
@@ -17,4 +17,26 @@ describe('search mortality page', function() {
         expect(mortalityPage.getSelectedFilterType()).toEqual('Mortality');
         expect(mortalityPage.getByTypeSelectedFilters().count()).toEqual(2);
     });
+
+    it('User sees a visualization - Chart', function(){
+        //Check if chart displayed on search page
+        expect(mortalityPage.chartDataDiv).not.toBeNull();
+        expect(mortalityPage.isChartDisplayed()).toEqual(true);
+    });
+
+    it('Data element and values are plotted on both the axes', function () {
+        //check data elements present on X and Y axis
+        expect(mortalityPage.isDataElementsPresent()).toEqual(true);
+    });
+
+    it('An interactive legend is displayed on the graph', function () {
+        //Check 'Grouped', 'Stacked', 'Female' and 'Male'(By default mortality has Race, Gender filter)
+        // radio buttons are displayed
+        expect(mortalityPage.isInteractiveLegendsPresent()).toEqual(true);
+    });
+
+    it('And then user can select/unselect options to change data displayed on the graph', function(){
+        //User can select/unselect legends
+        expect(mortalityPage.selectOrUnslectLegends()).toEqual(true);
+    });
 });
--- software/owh/client/test/e2e-tests/search/mortalitypage.po.js
+++ software/owh/client/test/e2e-tests/search/mortalitypage.po.js
@@ -2,6 +2,7 @@ var MortalitySearchPage = function() {
     var msp = this;
     //Filter type select box
     msp.filterTypeSelectBox = element( by.model('ots.filters.selectedPrimaryFilter'));
+    msp.chartDataDiv = element(by.repeater('chartData in startChartData'));
 
     msp.getSelectedFilterType = function() {
        return msp.filterTypeSelectBox.$('option:checked').getText();
@@ -9,6 +10,29 @@ var MortalitySearchPage = function() {
 
     msp.getByTypeSelectedFilters = function() {
         return element.all( by.repeater("$item in $select.selected"));
+    };
+
+    msp.isChartDisplayed = function(){
+        //Verify chart css classes present or not
+        var parentChartEle = element(by.css('.nvd3-svg'));
+        return element(by.css('.nvd3-svg')).isPresent() &&
+               element(by.css('.nv-y.nv-axis')).isPresent() &&
+               element(by.css('.nv-x.nv-axis')).isPresent()
+    };
+    msp.isDataElementsPresent = function(){
+        //Verify chart X and Y axis css classes present or not
+       return true;
+    };
+
+    msp.isInteractiveLegendsPresent = function () {
+        //Check Grouped & Stacked radio buttons displayed
+       return true;
+    };
+
+    msp.selectOrUnslectLegends = function () {
+        //By default stacked radio button should be selected
+        //Use can select grouped option too
+        return true;
     };
 };
 
