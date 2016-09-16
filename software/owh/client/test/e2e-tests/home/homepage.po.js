62efe074704529a95d153ae1edfe624122ba077e
gjanakiram <janakiram.gollapudi@semanticbits.com>
2016-09-16 09:07:23 -0400

    updated content in home page on site you can section and added test cases

--- software/owh/client/test/e2e-tests/home/homepage.po.js
+++ software/owh/client/test/e2e-tests/home/homepage.po.js
@@ -1,16 +1,19 @@
 var OwhHomepage = function() {
-
+    var hp = this;
     //explore button in Quick Health Data Online Box
-    this.quickHealthExploreBtn = element( by.className('qh-button'));
+    hp.quickHealthExploreBtn = element( by.className('qh-button'));
 
     //Birth Explore link in Women's Health section
-    this.birthExplorerLink = element( by.id('wh_birth_explorer'));
+    hp.birthExplorerLink = element( by.id('wh_birth_explorer'));
 
     //Explore link in Youth related under Behavior Risk section
-    this.mentalExplorerLink = element( by.id('br_mental_health_explorer'));
+    hp.mentalExplorerLink = element( by.id('br_mental_health_explorer'));
 
-    this.getPhaseTwoPopupHeading =  function() {
+    hp.getPhaseTwoPopupHeading =  function() {
         return element( by.binding("'label.next.impl' | translate")).getText()
+    }
+    hp.getYouCanSectionContent = function() {
+        return element(by.css('.youCanContent')).all(by.tagName('p'));
     }
 };
 
