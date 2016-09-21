85c4158eca0614b7475182ceb5b70f33c7846cb1
Joseph Jones <joseph.jones@semanticbits.com>
2016-09-21 11:55:22 -0400

    OWH-33: revert local dev testing code

--- software/owh/server/models/elasticSearch.js
+++ software/owh/server/models/elasticSearch.js
@@ -6,7 +6,7 @@ var Q = require('q');
 
 var _host = "https://search-spl-neoxainmajvric7meirttqquqy.us-east-1.es.amazonaws.com/";
 //To access local elastic search from Travis-CI
-if(process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'dev' || true){
+if(process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'dev'){
     _host = "http://localhost:9200/";
 }
 
