efeff49ca1bac80cee5c2fc90b087318791407c8
Ben McMeen <ben.mcmeen@semanticbits.com>
2016-09-22 09:10:53 -0500

    More first run issues.

--- software/owh/scripts/kill_container.sh
+++ software/owh/scripts/kill_container.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-#set -e
+set -e
 echo "** Killing containers ***"
+# This is to prevent first time run failures...
+mkdir -p /usr/local/owh/codebase/software/owh
 cd /usr/local/owh/codebase/software/owh
 echo "- Executing in /tmp/deploy -"
 ls
