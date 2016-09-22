4445af4e9f5c0b5744a48c1fa59a97a937364b80
Ben McMeen <ben.mcmeen@semanticbits.com>
2016-09-22 08:59:17 -0500

    Fixed script to create dir.

--- software/owh/scripts/load_images.sh
+++ software/owh/scripts/load_images.sh
@@ -5,6 +5,7 @@ echo "** Loading images ***"
 cd /tmp/deploy
 echo "- Executing in /tmp/deploy -"
 echo "checking out code from git................"
+mkdir -p /usr/local/owh/codebase
 cd  /usr/local/owh/codebase
 git pull
 echo "***Checkout completed successfully***"
