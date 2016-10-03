9688378558c0cba645b71726905b41f1473ad6ed
Gopal Unnikrishnan <gopal.unnikrishnan@semanticbits.com>
2016-10-03 07:57:49 -0500

    OWH-7: test implementation-fixes

--- software/owh/server/test/rserve_test.js
+++ software/owh/server/test/rserve_test.js
@@ -1,14 +1,16 @@
 var rserve = require("../api/rserve");
-var expect = require("expect");
+var expect = require("expect.js");
 
 describe("Rserve", function () {
-    it("execute simple R script"){
+    it("execute simple R script", function (done){
         var resp = rserve.executeRScript("R.version.string")
         expect(resp).to.be("R version 3.0.2 (2013-09-25)");
-    }
+        done()
+    })
 
-    it("execute R server function"){
+    it("execute R server function", function (done){
         var resp = rserve.executeRServerFunction("testFunction")
         expect(resp).to.be("R version 3.0.2 (2013-09-25)");
-    }
+        done()
+    })
 })
