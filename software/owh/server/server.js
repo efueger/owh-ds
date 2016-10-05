45a860d6946d227de09b19610cc375fc817c9de2
Gopal Unnikrishnan <gopal.unnikrishnan@semanticbits.com>
2016-10-05 16:18:27 -0500

    OWH-202: removed a duplicate line

--- software/owh/server/server.js
+++ software/owh/server/server.js
@@ -4,7 +4,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var bodyParser = require('body-parser');
 
 var app = express();
 app.use(bodyParser.json({limit: '5mb'}));
