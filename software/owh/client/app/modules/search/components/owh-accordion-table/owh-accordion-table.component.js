40fdea15afcd02dcda5ccbf9b01dc90506eaa64a
Jesse Lovelace <jesse.lovelace@gmail.com>
2016-10-20 22:59:27 -0500

    Add support to show/hide only one category.

--- software/owh/client/app/modules/search/components/owh-accordion-table/owh-accordion-table.component.js
+++ software/owh/client/app/modules/search/components/owh-accordion-table/owh-accordion-table.component.js
@@ -14,6 +14,7 @@
     OWHAccordionTableController.$inject = ['$scope', 'utilService', '$rootScope'];
     function OWHAccordionTableController($scope, utilService, $rootScope) {
         var oatc = this;
+        oatc.categoryFilter = null;
 
         oatc.collapseRow = function(row) {
             row.collapse = true;
@@ -29,6 +30,20 @@
 
         oatc.showLess = function(row) {
             row.hide = true;
+        };
+
+        oatc.filterCategory = function(value, index, array) {
+            if (oatc.categoryFilter == null) {
+                return true;
+            } else if (value == oatc.categoryFilter) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        oatc.showOnly = function(category) {
+            oatc.categoryFilter = category;
         };
 
     }
