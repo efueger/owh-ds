cafb9e912b0efa2bf97adb724d1630e03ee38b44
Joseph Jones <joseph.jones@semanticbits.com>
2016-11-11 11:59:57 -0500

    OWH-298: make sure other totals are blank in e2e test

--- software/owh/client/test/e2e-tests/features/step_definitions/mortalitySteps.js
+++ software/owh/client/test/e2e-tests/features/step_definitions/mortalitySteps.js
@@ -249,6 +249,8 @@ var mortalityStepDefinitionsWrapper = function () {
     this.Then(/^user should only see total for white race in side filter$/, function () {
         mortalityPage.getSideFilterTotals().then(function(elements) {
             expect(elements[18].getInnerHtml()).to.eventually.equal('611');
+            expect(elements[19].getInnerHtml()).to.eventually.equal('');
+            expect(elements[20].getInnerHtml()).to.eventually.equal('');
         });
     });
 
