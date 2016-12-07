c53266059484900f4976699385376b3e00d6b08a
gjanakiram <janakiram.gollapudi@semanticbits.com>
2016-12-07 15:49:29 -0500

    fixed failing unit tests

--- software/owh/client/app/components/owh-table/owh-table.component.spec.js
+++ software/owh/client/app/components/owh-table/owh-table.component.spec.js
@@ -28,6 +28,7 @@ describe('owhTable component: ', function() {
         $httpBackend.whenGET('app/i18n/messages-en.json').respond({ hello: 'World' });
         $httpBackend.whenGET('app/partials/marker-template.html').respond( $templateCache.get('app/partials/marker-template.html'));
         $httpBackend.whenGET('app/components/owh-table/owhTable.html').respond( $templateCache.get('app/components/owh-table/owhTable.html'));
+        $httpBackend.whenGET('/getFBAppID').respond({data: { fbAppID: 1111111111111111}});
     });
 
     it('should have a row of headers for each column header', function() {
