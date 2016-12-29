6b1b63001bed69081d5f7af75dd9352496947c4d
Gopal Unnikrishnan <gopal.unnikrishnan@semanticbits.com>
2016-12-29 11:34:04 -0600

    OWH-561: Log to both stout and logfile

--- software/owh/server/config/logging.js
+++ software/owh/server/config/logging.js
@@ -3,11 +3,17 @@ var config = require('./config');
 module.exports = bunyan.createLogger({
     // Writes daily rotating logs in folder $OWH_HOME/logs
     name: 'owh',
-    streams: [{
-        type: 'rotating-file',
-        path: config.OWH_HOME+'/logs/owh.log',
-        period: '1d',
-        count: 10,
-        level: config.logging.level
-    }]
+    streams: [
+        { // Log to file
+            type: 'rotating-file',
+            path: config.OWH_HOME+'/logs/owh.log',
+            period: '1d',
+            count: 10,
+            level: config.logging.level
+        },
+        { // Log to STDOUT
+            level: config.logging.level,
+            stream: process.stdout
+        }
+    ]
 });
