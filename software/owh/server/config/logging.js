ae5abd992beb800dfdcf5d5a88d5b356d8cacb20
Gopal Unnikrishnan <gopal.unnikrishnan@semanticbits.com>
2016-12-29 11:19:17 -0600

    OWH-561: fixed logging

--- software/owh/server/config/logging.js
+++ software/owh/server/config/logging.js
@@ -3,8 +3,11 @@ var config = require('./config');
 module.exports = bunyan.createLogger({
     // Writes daily rotating logs in folder $OWH_HOME/logs
     name: 'owh',
-    type: 'rotating-file',
-    period: '1d',
-    path: config.OWH_HOME+'/logs/owh.log',
-    level: config.logging.level
+    streams: [{
+        type: 'rotating-file',
+        path: config.OWH_HOME+'/logs/owh.log',
+        period: '1d',
+        count: 10,
+        level: config.logging.level
+    }]
 });
