d7dfb51095fdbc99a02eea09af869e0a4cb487f6
JanakiRam <janakiram.gollapudi@semanticbits.com>
2017-03-22 14:20:17 -0400

    added server test cases and fixed existing test. Removed should package and used expect

--- software/owh/server/test/elasticQueryBuilder_test.js
+++ software/owh/server/test/elasticQueryBuilder_test.js
@@ -1,17 +1,16 @@
 var elasticQueryBuilder = require("../api/elasticQueryBuilder");
 var supertest = require("supertest");
-var should = require("should");
 var expect = require("expect.js");
 
 describe("Build elastic search queries", function(){
-    it("Build search query with empty query and aggregations", function(done){
+     it("Build search query with empty query and aggregations", function(done){
         var params = {query:{}, aggregations:{}};
         var result = elasticQueryBuilder.buildSearchQuery(params, true)
         var query = result[0];
-        should(query).have.property('aggregations', {});
-        should(query).have.property('size', 0);
-        should(result[1]).be.not.ok;
-
+        expect(JSON.stringify(query.aggregations)).equal("{}");
+        expect(query.size).equal(0);
+        console.log(" result 1 ", result[1]);
+        expect(result[1]).equal(undefined);
         done()
     });
 
@@ -19,18 +18,19 @@ describe("Build elastic search queries", function(){
         var params = {query:{}, aggregations:{ simple:[{ key:"year", queryKey:"current_year", size:100000}]}};
         var result = elasticQueryBuilder.buildSearchQuery(params, true);
         var query = result[0];
-        should(query).have.property('aggregations', {"year":{"terms":{"field":"current_year","size":100000}}});
-        should(query).have.properties('query', 'size');
-        should(query).have.property('size', 0);
-        should(result[1]).be.not.ok;
+        expect(JSON.stringify(query.aggregations)).equal(JSON.stringify({"year":{"terms":{"field":"current_year","size":100000}}}));
+        expect(query.size).equal(0);
+        expect(query.query).to.not.equal(undefined);
+        expect(result[1]).equal(undefined);
         done()
     });
-    it("Build search query with empty query and simple and nested aggregations", function(done){
+
+     it("Build search query with empty query and simple and nested aggregations", function(done){
         var params = {query:{}, aggregations:{ simple:[{ key:"year", queryKey:"current_year", size:100000}], nested:[]}};
         var query = elasticQueryBuilder.buildSearchQuery(params, true)[0];
-        should(query).have.property('aggregations', {"year":{"terms":{"field":"current_year","size":100000}}});
-        should(query).have.properties('query', 'size');
-        should(query).have.property('size', 0);
+        expect(JSON.stringify(query.aggregations)).equal(JSON.stringify({"year":{"terms":{"field":"current_year","size":100000}}}));
+        expect(query.size).equal(0);
+        expect(query.query).to.not.equal(undefined);
         done()
     });
 
@@ -47,19 +47,24 @@ describe("Build elastic search queries", function(){
         var result = elasticQueryBuilder.buildSearchQuery(params, true);
         var query =result[0];
         var censusQuery = result[1];
-        should(query.aggregations).have.properties('year', 'group_table_gender', 'group_chart_0_gender','group_maps_0_states');
-        should(query.aggregations.group_table_gender.aggregations).have.properties('group_table_race');
-        should(query.aggregations.group_table_gender.aggregations.group_table_race.aggregations).have.properties('group_table_year');
-        should(query).have.properties('query', 'size');
-        should(query).have.property('size', 0);
-        should(censusQuery).be.ok;
-        should(censusQuery.aggregations).have.properties('group_table_gender');
-        should(censusQuery.aggregations.group_table_gender.aggregations).have.properties('group_table_race');
-        should(censusQuery.aggregations.group_table_gender.aggregations.group_table_race.aggregations).have.properties('group_table_year');
-        should(censusQuery.aggregations.group_table_gender.aggregations.group_table_race.aggregations.group_table_year.aggregations).have.properties('pop');
-        should(censusQuery.aggregations.group_table_gender.aggregations.group_table_race.aggregations.group_table_year.aggregations.pop.sum.field).be.equal("pop");
+        expect(query.aggregations).to.have.property('year');
+        expect(query.aggregations).to.have.property('group_table_gender');
+        expect(query.aggregations).to.have.property('group_chart_0_gender');
+        expect(query.aggregations).to.have.property('group_maps_0_states');
+        expect(query.aggregations.group_table_gender.aggregations).to.have.property('group_table_race');
+        expect(query.aggregations.group_table_gender.aggregations.group_table_race.aggregations).to.have.property('group_table_year');
+        expect(query.size).equal(0);
+        expect(query).to.have.property('query');
+        expect(query).to.have.property('size');
+        expect(censusQuery).to.not.equal(undefined);
+        expect(censusQuery.aggregations).to.have.property('group_table_gender');
+        expect(censusQuery.aggregations.group_table_gender.aggregations).to.have.property('group_table_race');
+        expect(censusQuery.aggregations.group_table_gender.aggregations.group_table_race.aggregations).to.have.property('group_table_year');
+        expect(censusQuery.aggregations.group_table_gender.aggregations.group_table_race.aggregations.group_table_year.aggregations).to.have.property('pop');
+        expect(censusQuery.aggregations.group_table_gender.aggregations.group_table_race.aggregations.group_table_year.aggregations.pop.sum.field).equal("pop");
         done()
     });
+
 
     it("Build search query with nested aggregations and count results by pop", function(done){
         var params = {countQueryKey:'pop', aggregations:{ simple:[{ key:"year", queryKey:"current_year",  size:10}],
@@ -72,15 +77,17 @@ describe("Build elastic search queries", function(){
         };
         var result = elasticQueryBuilder.buildSearchQuery(params, true);
         var query =result[0];
-        should(query.aggregations).have.properties('year', 'group_table_gender', 'group_count_pop', 'group_chart_0_gender');
-        should(query.aggregations.group_table_gender.aggregations).have.properties('group_table_race');
-        should(query.aggregations.group_chart_0_gender.aggregations).have.properties('group_chart_0_race');
-        should(query.aggregations.group_table_gender.aggregations.group_table_race.aggregations).have.properties('group_table_year');
-
+        expect(query.aggregations).to.have.property('year');
+        expect(query.aggregations).to.have.property('group_table_gender');
+        expect(query.aggregations).to.have.property('group_chart_0_gender');
+        expect(query.aggregations).to.have.property('group_count_pop');
+        expect(query.aggregations.group_table_gender.aggregations).to.have.property('group_table_race');
+        expect(query.aggregations.group_chart_0_gender.aggregations).to.have.property('group_chart_0_race');
+        expect(query.aggregations.group_table_gender.aggregations.group_table_race.aggregations).to.have.property('group_table_year');
         done()
     });
 
-    it("Build search query with query", function(done){
+     it("Build search query with query", function(done){
         var params = {
             "query":{
                 "current_year":{"key":"year","queryKey":"current_year","value":"2014","primary":false, caseChange:false},
@@ -97,13 +104,13 @@ describe("Build elastic search queries", function(){
                 "dummy":{"key":"dummy","queryKey":"","value":["N"],"primary":true, caseChange:true}
             }, aggregations:{}, pagination:{from:0, size:0}};
         var query = elasticQueryBuilder.buildSearchQuery(params, false);
-        should(query[0]).be.ok;
-        should(query[1]).not.be.ok;
-        /*console.log("query")
-         console.log(JSON.stringify(query))
-         console.log("query")*/
+        var queryZero = {"filtered":{"query":{"bool":{"must":[{"bool":{"should":[{"match":{"race":"1"}}]}},{"bool":{"should":[{"range":{"start_date":{"gte":"M","lte":"M"}}}]}},{"bool":{"should":[{"match":{"autopsy_data":"n"}}]}},{"bool":{}}]}},"filter":{"bool":{"must":[{"bool":{"should":[{"term":{"current_year":"2014"}}]}},{"bool":{"should":[{"term":{"gender":"m"}}]}},{"bool":{"should":[{"range":{"start_date":{"lte":"now"}}}]}},{"bool":{"should":[{"term":{"week_of_death":"6"}}]}},{"bool":{"should":[{"term":{"month_of_death":"12"}}]}}]}}}}
+        expect(query[0]).to.not.equal(undefined);
+        expect(JSON.stringify(query[0].query)).to.equal(JSON.stringify(queryZero));
+        expect(query[1]).equal(undefined);
         done()
     });
+
 
     it("Build search query with empty primary query and non-empty filter query", function(done){
         var params = {
@@ -114,37 +121,39 @@ describe("Build elastic search queries", function(){
                 "week_of_death":{"key":"weekday","queryKey":"week_of_death","value":"6","primary":false, caseChange:true}
             }, aggregations:{}, pagination:{from:0, size:0}};
         var query = elasticQueryBuilder.buildSearchQuery(params, false);
-        should(query[0]).be.ok;
-        should(query[1]).not.be.ok;
-        /*console.log("query")
-         console.log(JSON.stringify(query))
-         console.log("query")*/
+        console.log("query zero ", JSON.stringify(query[0]));
+        var queryZero = {"filtered":{"query":{"bool":{"must":[]}},"filter":{"bool":{"must":[{"bool":{"should":[{"term":{"current_year":"2014"}}]}},{"bool":{"should":[{"term":{"gender":"m"}}]}},{"bool":{"should":[{"range":{"start_date":{"lte":"now"}}}]}},{"bool":{"should":[{"term":{"week_of_death":"6"}}]}}]}}}};
+        expect(query[0]).to.not.equal(undefined);
+        expect(JSON.stringify(query[0].query)).to.equal(JSON.stringify(queryZero));
+        expect(query[1]).equal(undefined);
         done()
     });
+
+
     it("Build search query with empty invalid query key", function(done){
         var params = {
             "query":{
                 "current_year":{"key":"year","queryKey": new Object(),"value":"2014","primary":false, caseChange:false}
             }, aggregations:{}, pagination:{from:0, size:0}};
         var query = elasticQueryBuilder.buildSearchQuery(params, false);
-        should(query[0]).be.ok;
-        should(query[1]).not.be.ok;
-        /*console.log("query")
-         console.log(JSON.stringify(query))
-         console.log("query")*/
+        console.log("query zero ", JSON.stringify(query[0]));
+        var queryZero = {"filtered":{"query":{"bool":{"must":[]}},"filter":{"bool":{"must":[{"bool":{}}]}}}};
+        expect(query[0]).to.not.equal(undefined);
+        expect(JSON.stringify(query[0].query)).to.equal(JSON.stringify(queryZero));
+        expect(query[1]).equal(undefined);
         done()
     });
 
-    it("getGroupQuery returns query with size 0", function(done){
+     it("getGroupQuery returns query with size 0", function(done){
         var filter = {"key":"year","queryKey": "current_year","value":"2014","primary":false};
         var query = elasticQueryBuilder.getGroupQuery(filter);
-        should(query == {key: "year", queryKey: "current_year", getPercent: undefined, size: 0}).be.ok;
+        expect(JSON.stringify(query)).equal(JSON.stringify({key: "year", queryKey: "current_year", getPercent: undefined, size: 0}));
         done()
     });
 
-    it("prepareMapAggregations returns query with size 0", function(done){
+     it("prepareMapAggregations returns query with size 0", function(done){
         var query = elasticQueryBuilder.prepareMapAggregations();
-        should(query == [{ key: 'states', queryKey: 'state', size: 0 },{ key: 'sex', queryKey: 'sex', size: 0 }]).be.ok;
+        expect(JSON.stringify(query)).equal(JSON.stringify([[{ key: 'states', queryKey: 'state', size: 0 },{ key: 'sex', queryKey: 'sex', size: 0 }]]));
         done()
     });
 
